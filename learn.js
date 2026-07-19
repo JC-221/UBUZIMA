1748ed80b09749656ea40a63c656e6c6d97929a7
JC-221 <mugishaclaudius6@gmail.com>
2026-07-19 17:09:37 +0200

    Delete learn.js

--- learn.js
+++
@@ -1,9 +0,0 @@
-function toggleLesson(btn){
-    let content = btn.nextElementSibling;
-
-    document.querySelectorAll('.uh-lesson-content').forEach(el => {
-        if (el !== content) el.classList.remove("show");
-    });
-
-    content.classList.toggle("show");
-}
